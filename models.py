296ed8838493dcbc4f8e9d807fee6e0f31b38f7e
Arjun Agarwal <arjunagarwal771@gmail.com>
2026-05-07 01:21:48 +0530

    add authentication foundation

--- models.py
+++ models.py
@@ -6,3 +6,12 @@ class Teaching(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     category: str
+
+class Admin(SQLModel, table=True):
+    id: int | None = Field(
+        default=None,
+        primary_key=True
+    )
+
+    username: str
+    password: str
