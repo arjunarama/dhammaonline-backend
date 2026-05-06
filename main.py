d96e59cf99afabbfc72ebf11e586e0a140b7dfef
Arjun Agarwal <arjunagarwal771@gmail.com>
2026-05-07 00:45:50 +0530

    temporarily allow all cross origins

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ origins = [
     "http://localhost:3000",
     "http://localhost:3001",
     "https://dhammaonline.vercel.app",
+    "https://dhammaonline-admin.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
